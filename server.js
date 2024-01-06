8b0d671fb0f83f4bdaffec6abe7d83e697241ae2
Baeo Maltinsky <baeo.maltinsky@gmail.com>
2024-01-06 14:26:27 -0800

    save file names properly

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
     cb(null, dir) 
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    cb(null, `${(new Date()).toISOString()}-${file.originalname}`)
   }
 });
 
